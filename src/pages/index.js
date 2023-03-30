69233ff30aaf45828fc484e90ef9dc3c0cddf4fb
hardisaw <hardisaw@gmail.com>
2023-03-30 13:35:38 +0700

    add island pic and image plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const HomePage = () => {
   return (
@@ -8,6 +9,7 @@ const HomePage = () => {
       pageTitle='Home Page'
       >
         <p>Welcome to My Home Page MTF</p>
+        <StaticImage src="https://images.unsplash.com/photo-1502085671122-2d218cd434e6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1852&q=80" alt="A Island" />
       </Layout>
   )
 }
