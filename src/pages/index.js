fa4b0f5edfd7d7ad6c37ba25f422275f64018010
hardisaw <hardisaw@gmail.com>
2023-03-30 14:06:01 +0700

    add island images new

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,11 @@ const HomePage = () => {
       pageTitle='Home Page'
       >
         <p>Welcome to My Home Page MTF</p>
-        <StaticImage src="https://images.unsplash.com/photo-1502085671122-2d218cd434e6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1852&q=80" alt="A Island" />
+        <StaticImage src="https://images.unsplash.com/photo-1502085671122-2d218cd434e6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1852&q=80" 
+        alt="A Island"
+        loading="eager"
+        layout="constrained"
+        />
       </Layout>
   )
 }
